f5725b9deb138dfa0b046d6980aa2bf1fb89b353
Danish Pathan <animestaan@gmail.com>
2023-03-01 21:42:57 +0530

    addedreset

--- app.js
+++ app.js
@@ -8,13 +8,25 @@ let p2healsound = document.getElementById('p2heal')
 let p1HealthDiv = document.getElementById("p1Health")
 let p2HealthDiv = document.getElementById("p2Health")
 let resultDiv = document.getElementById('result')
+let resetBtn = document.getElementById('resetBtn')
 // let p2Attack = document.getElementById('p2Attack')
+class Person {
+    name;
+    health;
+    constructor(health){
+        this.health = health
+    }
+}
+let player1 = new Person(p1HealthDiv.textContent)
+let player2 = new Person(p2HealthDiv.textContent)
 console.log(p1atksound);
-
+console.log(player2.name);
+console.log(player1.health);
+console.log(player2.health);
 // console.log(p1Name);
-let p2Health = p2HealthDiv.textContent;
 
-let p1Health = p1HealthDiv.textContent;
+
+
 console.log();
 let randomAttack = () => {
    return Math.floor(Math.random()*10)
@@ -39,59 +51,77 @@ document.addEventListener('keydown',function(e){
     }
 })
 const p2Attack =()=> {
-let p1Name = p1NameDiv.innerText;
-if(p2Health<=0){
-    Winner(p1Name)
-}
+player2.name = p2NameDiv.innerText;
+console.log(player2.name);
+
+    Winner()
+
     let health = randomAttack();
-    p1Health-=health
+    player1.health-=health
     p2atksound.play();
-    p1HealthDiv.innerHTML=`<h1>${p1Health}</h1>`
+    p1HealthDiv.innerHTML=`<h1>${player1.health}</h1>`
 }
 const p1Attack= () => {
     let health = randomAttack();
     let p2Name = p2NameDiv.innerText;
     console.log(p2Name);
     
-    p2Health-=health;
+    player2.health-=health;
     p1atksound.play()
-    if(p1Health<=0 ){
-        Winner(p2Name)
-   }
-    p2HealthDiv.innerHTML = `<h1>${p2Health}</h1>`
+    
+        Winner()
+   
+    p2HealthDiv.innerHTML = `<h1>${player2.health}</h1>`
     
 }
 const p1Heal= () => {
    
     let healHealth = randomHeal();
     p1healsound.play()
-    if(p1Health<100){
-        p1Health+=healHealth;
+    if(player1.health<100){
+        player1.health+=healHealth;
     }
-    if(p1Health>100){
-        p1Health=100;
+    if(player1.health>100){
+        player1.health=100;
     }
  
-    p1HealthDiv.innerHTML =`<h1>${p1Health}</h1>`
+    p1HealthDiv.innerHTML =`<h1>${player1.health}</h1>`
     
 
 }
 const p2Heal= () => {
     let healHealth = randomHeal();
     p2healsound.play()
-    if(p2Health<100){
-        p2Health+=healHealth;
+    if(player2.health<100){
+        player2.health+=healHealth;
     }
-    if(p2Health>100){
-        p2Health=100;
+    if(player2.health>100){
+        player2.health=100;
     }
    
-    p2HealthDiv.innerHTML =`<h1>${p2Health}</h1>`
+    p2HealthDiv.innerHTML =`<h1>${player2.health}</h1>`
 }
 
 const Winner = 
-(name)=> {
-        resultDiv.innerHTML = `<h1 style='color:green'> Winner: ${name}</h1>`
+()=> {
+    if(player1.health<=0){
+        resultDiv.innerHTML = `<h1 style='color:green'> Winner: ${player2.name}</h1>`
+
+    }
+    else if(player2.health<=0){
+        resultDiv.innerHTML = `<h1 style='color:green>Winner :${player1.name
+        }`
+
+    }
+        
 
     
+
+    
+}
+const reset= () => {
+    player1.health=100;
+    player2.health=100;
+    p1HealthDiv.innerHTML=`<h1>${player1.health}</h1>`
+    p2HealthDiv.innerHTML=`<h1>${player2.health}</h1>`
 }
